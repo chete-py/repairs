4b6bb8a8f18e020a37b92c57e533297cec4f686d
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2023-12-11 16:40:38 +0300

    Update repairs.py

--- repairs.py
+++ repairs.py
@@ -248,11 +248,11 @@ def main():
         
         elif selected_option == "With Date":
             # Filter the DataFrame to include rows with dates
-            final_df = df[df['Date Repair Authorized'].notnull() == selected_option]
+            final_df = df[df['date_authorized'].notnull() == selected_option]
         
         elif selected_option == "Without Date":
             # Filter the DataFrame to include rows without dates (blanks)
-            final_df = df[df['Date Repair Authorized'].isnull() == selected_option]    
+            final_df = df[df['date_authorized'].isnull() == selected_option]    
     
         edited_df = st.data_editor(final_df)
     
