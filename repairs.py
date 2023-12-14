3b18bf319d7e8cfc255c6d1d5918e6f347a7a2b7
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2023-12-14 12:34:02 +0300

    Update repairs.py

--- repairs.py
+++ repairs.py
@@ -158,17 +158,17 @@ def main():
     
         assessor = st.selectbox("Assessor", ["EXPRESS", "KEVO", "NOT ASSESSED", "TPPD", "T.B.A"])
 
-        assessor_appointed = st.text_input("Date Assessor Appointed")  
+        assessor_appointed = st.date_input("Date Assessor Appointed")  
 
         report_received = st.text_input("Date Report Received")  
 
         outcome = st.selectbox("Claim Outcome", ["REPAIRABLE", "THIRD PARTY PURPOSES", "PENDING", "WRITE-OFF", "BELOW EXCESS", "UNDER INVESTIGATION", "CLAIM DECLINED", "RELEASED"])
 
-        date_authorized = st.text_input("Date Repair Authorized")
+        date_authorized = st.date_input("Date Repair Authorized")
 
         repair_amount = st.text_input("Repair Amount") 
     
-        release_date = st.text_input("Release Date")  
+        release_date = st.date_input("Release Date")  
     
     
     
